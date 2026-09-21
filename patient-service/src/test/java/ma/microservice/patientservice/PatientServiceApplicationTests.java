3e6f210fbd5b4ca79a6fd95774b79a6cc9ce0c89
zikohaourir <zikohaourir@gmail.com>
2026-09-21 16:50:00 +0100

    Initial commit

---
+++ patient-service/src/test/java/ma/microservice/patientservice/PatientServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.microservice.patientservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
