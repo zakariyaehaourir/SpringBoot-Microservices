eec840503db2a8427c246eaa9fa391288f75843a
zikohaourir <zikohaourir@gmail.com>
2026-09-21 20:15:17 +0100

    Adds Patient model with h2 memory DB

---
+++ patient-service/src/main/java/ma/microservice/patientservice/model/Patient.java
@@ -0,0 +1,41 @@
+package ma.microservice.patientservice.model;
+
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Data
+public class Patient {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private UUID id;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    private String address;
+
+    @NotNull
+    @Email
+    @Column(unique = true)
+    private String email;
+
+    @NotNull
+    private LocalDate birthDate;
+
+    @NotNull
+    @NotBlank
+    private LocalDateTime registeredAt;
+
+
+
+}
